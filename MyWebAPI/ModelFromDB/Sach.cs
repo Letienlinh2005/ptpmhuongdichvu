7c51670ec1369e2d84e6142d7e234e432469f6fa
HapCT <162394072+HapCT@users.noreply.github.com>
2025-09-18 16:26:26 +0700

    FE

---
+++ MyWebAPI/ModelFromDB/Sach.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWebAPI.ModelFromDB;
+
+[Table("Sach")]
+public partial class Sach
+{
+    [Key]
+    [StringLength(20)]
+    public string MaSach { get; set; } = null!;
+
+    [StringLength(255)]
+    public string TieuDe { get; set; } = null!;
+
+    [StringLength(255)]
+    public string TacGia { get; set; } = null!;
+
+    [StringLength(20)]
+    public string? MaTheLoai { get; set; }
+
+    public int? NamXuatBan { get; set; }
+
+    [StringLength(50)]
+    public string? NgonNgu { get; set; }
+
+    public string? TomTat { get; set; }
+
+    [InverseProperty("MaSachNavigation")]
+    public virtual ICollection<BanSao> BanSaos { get; set; } = new List<BanSao>();
+
+    [InverseProperty("MaSachNavigation")]
+    public virtual ICollection<DatCho> DatChos { get; set; } = new List<DatCho>();
+
+    [ForeignKey("MaTheLoai")]
+    [InverseProperty("Saches")]
+    public virtual TheLoai? MaTheLoaiNavigation { get; set; }
+}
---
+++ MyWebAPI/ModelFromDB/TaiKhoan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWebAPI.ModelFromDB;
+
+[Table("TaiKhoan")]
+[Index("TenDangNhap", Name = "UQ__TaiKhoan__55F68FC0EC635BE8", IsUnique = true)]
+public partial class TaiKhoan
+{
+    [Key]
+    [StringLength(20)]
+    public string MaTaiKhoan { get; set; } = null!;
+
+    [StringLength(100)]
+    public string TenDangNhap { get; set; } = null!;
+
+    [StringLength(255)]
+    public string MatKhau { get; set; } = null!;
+
+    [StringLength(20)]
+    public string VaiTro { get; set; } = null!;
+
+    [StringLength(20)]
+    public string? MaBanDoc { get; set; }
+
+    [ForeignKey("MaBanDoc")]
+    [InverseProperty("TaiKhoans")]
+    public virtual BanDoc? MaBanDocNavigation { get; set; }
+}
